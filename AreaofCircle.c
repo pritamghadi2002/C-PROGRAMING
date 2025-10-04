5cfea42309cfd57421e1d5d3ecfbd600af49b970
pritamghadi2002 <pritamghadai1@gmail.com>
2025-10-04 08:30:48 +0530

    Add files via upload

---
+++ AreaofCircle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    float r;
+    printf(" ENTER THE VLAUE OF RADIOUS :\n ");
+    scanf("%f" , &r);
+    printf(" THE ARE OF CIRCLE IS : %f" , 3.14 * r * r );
+    getch();
+    return 0;
+}

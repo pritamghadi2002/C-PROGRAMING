b9b4623f3bfcb65ed545586656ff92c8c86ec497
pritamghadi2002 <pritamghadai1@gmail.com>
2025-10-09 08:20:05 +0530

    Add files via upload

---
+++ number_reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int i=0;
+    printf("NUMBER FOR 1 TO 10 IS:\n");
+    for(int i=0; i<=10; i++){
+        printf(" %d ",i);    
+    }
+        printf("\nTHE REVERSE UNMBER IS:\n");
+        for (int i=10; i>=1; i--){
+            printf(" %d ",i);   
+        }
+
+    getch();
+
+}

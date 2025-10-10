1a80aea7e5aca0204395d2b9358dcc3021252a61
pritamghadi2002 <pritamghadai1@gmail.com>
2025-10-10 11:15:21 +0530

    Add files via upload

---
+++ factorial_of_1_to_usernumbr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int num, i,fact=1;
+    printf("Enter a number:\n");
+    scanf("%d",&num);
+    for (i=1; i<=num; i++)
+    {
+            fact=fact*i;
+       
+      
+    }
+    printf("The factorial of %d is %d, ",num,fact); 
+      getch();
+}

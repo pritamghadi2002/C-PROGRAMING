2d5a7a8632b0f7c8db42af6dd64aa91eff9837ff
pritamghadi2002 <pritamghadai1@gmail.com>
2025-10-10 10:39:35 +0530

    Update percentage.c

--- percentage.c
+++ percentage.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 #include<conio.h>
-void main(){
-    int mark ,i,sum=0;
-    float per;
-    printf("enter mark of 5 subjects\n");
-    
-    for(i=0; i<5 ; i++){
 
-      scanf("%d",&mark);
+void main() {
+    int mark[5], i, sum = 0;
+    float per;
+
+    printf("Enter marks of 5 subjects:\n");
+
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &mark[i]);
     }
-    for(i=0; i<5 ; i++){
-        sum=sum+mark;
+
+    for(i = 0; i < 5; i++) {
+        sum = sum + mark[i];
     }
-    printf("Total mark is %d",sum);
-    per=(sum/500.0)*100;
-    printf("\nPercentage is %f",per);
-        
-   
-    return 0;
+
+    printf("Total marks = %d\n", sum);
+    per = (sum / 500.0) * 100;
+    printf("Percentage = %.2f%%", per);
+
+    getch();
 }
